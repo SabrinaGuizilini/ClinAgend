b03113e942b76e721deee271410b863ab7634aed
SabrinaGuizilini <sabrinaoliveira030989@gmail.com>
2026-05-27 15:48:40 -0300

    Alterações para máscaras funcionarem corretamente no ambiente publicado

---
+++ ClinAgend.Web/Shared/Enums/MaskType.cs
@@ -0,0 +1,12 @@
+﻿namespace ClinAgend.Web.Shared.Enums;
+
+public enum MaskType
+{
+    None,
+    Phone,
+    CPF,
+    CNPJ,
+    CPFCNPJ,
+    Time,
+    Date
+}
---
+++ ClinAgend.Web/wwwroot/js/masks.js
@@ -0,0 +1,54 @@
+﻿window.maskFunctions = {
+
+    apply: (elementId, maskType) => {
+
+        const element =
+            document.getElementById(elementId);
+
+        if (!element)
+            return;
+
+        switch (maskType) {
+
+            case "Phone":
+
+                IMask(element, {
+                    mask: '(00)00000-0000'
+                });
+
+                break;
+
+            case "CPF":
+
+                IMask(element, {
+                    mask: '000.000.000-00'
+                });
+
+                break;
+
+            case "CNPJ":
+
+                IMask(element, {
+                    mask: '00.000.000/0000-00'
+                });
+
+                break;
+
+            case "Time":
+
+                IMask(element, {
+                    mask: '00:00'
+                });
+
+                break;
+
+            case "Date":
+
+                IMask(element, {
+                    mask: '00/00/0000'
+                });
+
+                break;
+        }
+    }
+};
